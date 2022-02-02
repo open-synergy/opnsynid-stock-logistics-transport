65a2c2f0d4b9629ef40ac3d4e495853db91ec5af
mikevhe18 <viriyananda.michael@gmail.com>
2022-02-02 11:32:33 +0700

    [stock-logistics-transport] pre-commit fix

--- stock_transport_notify_party/models/stock_move.py
+++ stock_transport_notify_party/models/stock_move.py
@@ -2,7 +2,7 @@
 # Copyright 2017 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api
+from openerp import api, models
 
 
 class StockMove(models.Model):
@@ -11,16 +11,16 @@ class StockMove(models.Model):
     @api.multi
     def _picking_assign(self, procurement_group, location_from, location_to):
         _super = super(StockMove, self)
-        res = _super._picking_assign(
-            procurement_group, location_from, location_to)
+        res = _super._picking_assign(procurement_group, location_from, location_to)
         for picking in self.mapped("picking_id"):
             group = picking.group_id
             if not group:
                 continue
             changes = {}
             if not picking.notify_party_id:
-                changes["notify_party_id"] = group.notify_party_id and \
-                    group.notify_party_id.id or False
+                changes["notify_party_id"] = (
+                    group.notify_party_id and group.notify_party_id.id or False
+                )
             picking.write(changes)
         return res
 
